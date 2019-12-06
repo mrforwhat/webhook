d6495008f0c13fb4a5a6be4834cb007179ee65b7
gaopenghang <497236467@qq.com>
2019-12-06 13:40:44 +0800

    start

--- webhook.js
+++ webhook.js
@@ -1,6 +1,7 @@
 let http = require('http')
 let server = http.createServer((req, res) => {
   if (req.method == 'POST' && req.url == '/webhook') {
+    console.log(req.method,req.url)
     res.setHeader('Content-Type', 'application/json')
     res.end(JSON.stringify({ok: true}))
   } else {

0ebdb9849f9dcc9e0292eaa0100021eae5ead608
gaopenghang <497236467@qq.com>
2019-12-06 14:25:24 +0800

    docker

---
+++ blog-api.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+WORK_PATH= '/home/app/blog-api'
+cd$ $WORK_PATH
+echo "先清除老代码"
+git reset --hard origin/master
+git clean -f
+echo "拉取最新代码"
+git pull origin master
+echo "开始构建"
+docker build -t blog-api .
+echo "停止旧容器并删除旧容器"
+docker stop blog-api-container
+docker rm blog-api-container
+echo "启动新容器"
+docker container run -p 3000:3000 --name blog-api-container -d blog-api
--- webhook.js
+++ webhook.js
@@ -1,7 +1,25 @@
 let http = require('http')
+let crypto = require('crypto ')
+let SECRET = '123456'
+
+function sign(body) {
+  return `sha1=` + crypto.creatHmac('sha1', SECRET).update(body).digest('hex')
+}
 let server = http.createServer((req, res) => {
+  console.log(req.method,req.url)
   if (req.method == 'POST' && req.url == '/webhook') {
-    console.log(req.method,req.url)
+    let buffers = []
+    req.on('data',function (buffer) {
+      buffers.push(buffer)
+    })
+    req.on('end',function(buffer) {
+      let body = buffer.concat(buffers)
+      let event = req.header['x-gitHub-event']
+      let signature = req.header['X-hub-signature']
+      if(signature !== sign(body)) {
+        return res.end('Not Allowed')
+      }
+    })
     res.setHeader('Content-Type', 'application/json')
     res.end(JSON.stringify({ok: true}))
   } else {

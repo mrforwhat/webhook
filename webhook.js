81da28ef11530da441b21cb7f16ee31987a5f956
gaopenghang <497236467@qq.com>
2019-12-06 11:43:38 +0800

    'init'

---
+++ webhook.js
@@ -0,0 +1,13 @@
+let http = require('http')
+let server = http.createServer((req,res)=> {
+  if(req.method == 'POST' && req.url == '/webhook') {
+    res.setHeader('Content-Type','application/json')
+    res.end(JSON.stringify({ok:true}))
+  }else {
+    res.end('not found')
+  }
+})
+
+server.listen(4000,()=> {
+  console.log('webhook服务已经在4000端口上启动')_
+})

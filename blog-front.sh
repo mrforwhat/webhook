b82edacef5588776b41386eda11b723eec785112
gaopenghang <497236467@qq.com>
2019-12-10 13:25:13 +0800

    sh

--- blog-front.sh
+++ blog-front.sh
@@ -7,6 +7,8 @@ git reset --hard origin/master
 git clean -f
 echo "拉取最新代码"
 git pull origin master
+echo "安装依赖"
+npm install
 echo "编译打包"
 npm run build
 echo "开始构建"
@@ -15,4 +17,4 @@ echo "停止旧容器并删除旧容器"
 docker stop blog-front-container
 docker rm blog-front-container
 echo "启动新容器"
-docker container run -p 80:7 --name blog-front-container -d blog-front:1.0
+docker container run -p 80:80 --name blog-front-container -d blog-front:1.0
